db6bf64b2e73c364a9b7987ed7c17efe70527de6
jwhwang0911 <star4007lg@gmail.com>
2021-10-25 21:46:08 +0900

    Bytonic Series

---
+++ Dp/Series/bj_11054.py
@@ -0,0 +1,19 @@
+def main():
+    n = int(input())
+    li = list(map(int,input().split(" ")))
+    dp1 = [1 for col in range(n)]
+    dp2 = [1 for col in range(n)]
+    for i in range(n):
+        for j in range(i):
+            if li[i] > li[j]:
+                dp1[i] = max(dp1[j]+1,dp1[i])
+            if li[i] > li[n-i-1]:
+                dp2[i] = max(dp2[j]+1,dp2[n-i-1])
+    dp = []
+    for i in range(n):
+        dp.append(dp1[i]+dp2[i]-1)
+    print(max(dp))
+
+
+if __name__ == "__main__":
+    main()
